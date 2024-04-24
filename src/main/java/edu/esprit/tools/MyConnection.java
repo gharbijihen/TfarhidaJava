ef98bb732ff7580920ece95a9b0c7b34dcab1155
Tayssirsboui <tayssirsboui@gmail.com>
2024-04-24 23:20:21 +0100

    Commited

--- src/main/java/edu/esprit/tools/MyConnection.java
+++ src/main/java/edu/esprit/tools/MyConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
-    private String URL="jdbc:mysql://localhost:3306/tfarhida";
+    private String URL="jdbc:mysql://localhost:3306/testtest";
     private String USER="root";
     private String PWD="";
     Connection cnx;
