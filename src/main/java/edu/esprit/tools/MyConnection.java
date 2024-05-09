d368eabdcd34ce7acfb17c4e70402bd024499095
Tayssirsboui <tayssirsboui@gmail.com>
2024-05-09 20:08:13 +0100

    updates

--- src/main/java/edu/esprit/tools/MyConnection.java
+++ src/main/java/edu/esprit/tools/MyConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MyConnection {
+public class  MyConnection {
     private String URL="jdbc:mysql://localhost:3306/testtest";
     private String USER="root";
     private String PWD="";
